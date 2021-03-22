dae61a6189efe21f10abe5709637b4fe18806b13
Koszegi First <koszegi.proba9@gmail.com>
2021-03-22 21:34:56 +0100

    Ready for netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: "TM-Konti",
-      siteUrl: `vibrant-elion-16de38.netlify.app`,
+      siteUrl: `https://www.vibrant-elion-16de38.netlify.app`,
   },
   plugins: [
     {
