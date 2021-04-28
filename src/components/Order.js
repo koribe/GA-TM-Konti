aae64d5c7258c9a56a3dafa2cbf9152b6ecb4d90
Koszegi First <koszegi.proba9@gmail.com>
2021-04-28 21:53:01 +0200

    Order first styled

--- src/components/Order.js
+++ src/components/Order.js
@@ -3,24 +3,60 @@ import './styles/Order.css';
 import { useForm } from 'react-hook-form';
 
 function Order () {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
 
-  console.log(watch("example")); // watch input value by passing the name of it
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
-      
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-      
+    <section id="order">
+    <h1>Kapcsolat</h1>
+     {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+    <form id="order-form" onSubmit={handleSubmit(onSubmit)}>
+
+      <ul id="inputs-container">
+        <li className="input">
+      <label htmlFor="name">Név</label>
+      <div className="input-field">
+      <input
+        id="name"
+        {...register("name", {
+          required: "A mező kitöltése kötelező",
+          minLength: {
+            value: 3,
+            message: "Túl rövid a név"
+          },
+          maxLength: {
+            value:100,
+            message: "Túl hosszú a név"
+          }
+        })}
+      />
+
+      {errors.name && <small className="error-msg" role="alert">{errors.name.message}</small>}
+      </div>
+      </li>
+      <li className="input">
+      <label htmlFor="email">Email</label>
+      <div className="input-field">
+      <input
+        id="email"
+        {...register("email", {
+          required: "A mező kitöltése kötelező",
+          pattern: {
+            value: /\S+@\S+.\S+/,
+            message: "Kérjük, adja meg helyesen email címét!"
+          }
+        })}
+        type="email"
+      />
+
+      {errors.email && <small className="error-msg" role="alert">{errors.email.message}</small>}
+      </div>
+      </li>
+      </ul>
       <input type="submit" />
     </form>
+    </section>
   );
 }
 export default Order;
